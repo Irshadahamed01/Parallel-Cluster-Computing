797df099beda185fe7716fd275bf31198ce28249
Irshad Ahamed <118520491+irshadahamedmi@users.noreply.github.com>
2022-12-28 20:07:07 +0530

    Hamming distance
    MPI program to find the differences between two binary patterns and is called Hamming 
    distance. For example, the Hamming distance between binary pattern 1011101 and 1001001 is 2.
    Initially the process with id 0 has two sequences of binary numbers:
    Sequence 1: 11000011 10110110 01000001 00000110 11110101 10100010 01010101 00010100
    Sequence 2: 10001001 11011100 10010111 01000110 11011101 11101011 01111000 10111011
    a) Distribute the binary numbers among eight processes. Each process will receive 8 bits 
    binary pattern from sequence 1 and sequence 2.
    b) Each process including process with id 0 should print the 8-bit binary received.
    c) Each process including process with id 0 should calculate Hamming distance and 
    print it.
    d) Process with id 0 should collect hamming distance from all processes and print the 
    total hamming distance.

---
+++ Hamming distance.cpp
@@ -0,0 +1,160 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main() {
+    int np;
+    int pid;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Status sta;
+
+    int seq01[4][8];
+    int seq02[4][8];
+    if (pid == 0) {
+        seq01[0][0] = 1;
+        seq01[0][1] = 1;
+        seq01[0][2] = 0;
+        seq01[0][3] = 0;
+        seq01[0][4] = 0;
+        seq01[0][5] = 0;
+        seq01[0][6] = 1;
+        seq01[0][7] = 1;
+
+        seq01[1][0] = 1;
+        seq01[1][1] = 0;
+        seq01[1][2] = 1;
+        seq01[1][3] = 1;
+        seq01[1][4] = 0;
+        seq01[1][5] = 1;
+        seq01[1][6] = 1;
+        seq01[1][7] = 0;
+
+        seq01[2][0] = 0;
+        seq01[2][1] = 0;
+        seq01[2][2] = 0;
+        seq01[2][3] = 0;
+        seq01[2][4] = 0;
+        seq01[2][5] = 1;
+        seq01[2][6] = 1;
+        seq01[2][7] = 0;
+
+        seq01[3][0] = 1;
+        seq01[3][1] = 1;
+        seq01[3][2] = 1;
+        seq01[3][3] = 1;
+        seq01[3][4] = 0;
+        seq01[3][5] = 1;
+        seq01[3][6] = 0;
+        seq01[3][7] = 1;
+
+        seq02[0][0] = 1;
+        seq02[0][1] = 0;
+        seq02[0][2] = 0;
+        seq02[0][3] = 0;
+        seq02[0][4] = 1;
+        seq02[0][5] = 0;
+        seq02[0][6] = 0;
+        seq02[0][7] = 1;
+
+        seq02[1][0] = 1;
+        seq02[1][1] = 1;
+        seq02[1][2] = 0;
+        seq02[1][3] = 1;
+        seq02[1][4] = 1;
+        seq02[1][5] = 1;
+        seq02[1][6] = 0;
+        seq02[1][7] = 0;
+
+        seq02[2][0] = 0;
+        seq02[2][1] = 1;
+        seq02[2][2] = 0;
+        seq02[2][3] = 0;
+        seq02[2][4] = 0;
+        seq02[2][5] = 1;
+        seq02[2][6] = 1;
+        seq02[2][7] = 0;
+
+        seq02[3][0] = 1;
+        seq02[3][1] = 1;
+        seq02[3][2] = 0;
+        seq02[3][3] = 1;
+        seq02[3][4] = 1;
+        seq02[3][5] = 1;
+        seq02[3][6] = 0;
+        seq02[3][7] = 1;
+    }
+    int rec_seq01[8];
+    int rec_seq02[8];
+    int ham_dis = 0;
+    MPI_Scatter(&seq01, 8, MPI_INT, &rec_seq01, 8, MPI_INT,0, MPI_COMM_WORLD);
+    MPI_Scatter(&seq02, 8, MPI_INT, &rec_seq02, 8, MPI_INT,0, MPI_COMM_WORLD);
+
+    printf("\nMy Processor id is %d", pid);
+    printf("\nReceived Data from sequence 1 is: ");
+    for (int i = 0;i < 7;i++) {
+        printf("%d ", rec_seq01[i]);
+    }
+    printf("\nReceived Data from sequence 2 is: ");
+    for (int i = 0;i < 7;i++) {
+        printf("%d ", rec_seq02[i]);
+    }
+    printf("\nHamming Distance: ");
+    for (int i = 0;i < 7;i++) {
+        if (rec_seq01[i] != rec_seq02[i]) {
+            ham_dis++;
+        }
+    }
+    printf("%d", ham_dis);
+   
+    int ham[8];
+    int total = 0;
+    MPI_Gather(&ham_dis, 1, MPI_INT, &ham, 1, MPI_INT,0, MPI_COMM_WORLD);
+
+    if (pid == 0) {
+        printf("\nHamming Distances are: ");
+        for (int i = 0;i < 4;i++) {
+            total = total + ham[i];
+            printf("%d ", ham[i]);
+        }
+        printf("%d", total);
+    }
+    
+
+    printf("\n#############################################");
+
+    MPI_Finalize();
+    return 0;
+
+}
+
+/*
+
+
+C:\Users\rujha\Documents\Parellal Computing\Project1\x64\Debug > mpiexec - n 4 Project1.exe
+
+My Processor id is 1
+Received Data from sequence 1 is: 1 0 1 1 0 1 1
+Received Data from sequence 2 is : 1 1 0 1 1 1 0
+Hamming Distance : 4
+#############################################
+My Processor id is 2
+Received Data from sequence 1 is : 0 0 0 0 0 1 1
+Received Data from sequence 2 is : 0 1 0 0 0 1 1
+Hamming Distance : 1
+#############################################
+My Processor id is 3
+Received Data from sequence 1 is : 1 1 1 1 0 1 0
+Received Data from sequence 2 is : 1 1 0 1 1 1 0
+Hamming Distance : 2
+#############################################
+My Processor id is 0
+Received Data from sequence 1 is : 1 1 0 0 0 0 1
+Received Data from sequence 2 is : 1 0 0 0 1 0 0
+Hamming Distance : 3
+Hamming Distances are : 3 4 1 2 10
+#############################################
+
+
+*/
